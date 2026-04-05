6ed9f3a5cec0b223057b66394a1e5f91dc04c5f7
Miggy <miggy@orderform.local>
2026-04-05 11:06:28 +0800

    refactor: extract firebaseConfig to shared firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey:            "AIzaSyB7wYqbYn-55GtodOiJ2LF5hwygdGJ8g1I",
+  authDomain:        "cafe-order-form-40d98.firebaseapp.com",
+  projectId:         "cafe-order-form-40d98",
+  storageBucket:     "cafe-order-form-40d98.firebasestorage.app",
+  messagingSenderId: "767534130637",
+  appId:             "1:767534130637:web:7127dd48fd07c58a65d084",
+};
